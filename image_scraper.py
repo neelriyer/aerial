bd15fea543acc8e1e989f79c50e79ed29a8468a7
Neel-Iyer <neel.r.iyer@gmail.com>
2019-07-30 12:23:59 +1000

    init commit

--- image_scraper.py
+++ image_scraper.py
@@ -2,7 +2,7 @@
 image scraper from nearmap
 api call: "https://api.nearmap.com/tiles/v3/Vert/21/"+str(x)+"/"+str(y)+".jpg?apikey=ODI0M2IxNzEtMmJhMy00NGRkLWEzMTItYTE0ZDk1ZGFmMmI5"
 
-for google tiles cooridnates between:
+for google tiles cooridinates between:
 1855744<=x<=1855999
 1265792<=y<=1265983
 
